8385f15ab8a6ba8070cc51f0e5508566734404eb
zengxiaohui <zengxiaohui@bytedance.com>
2025-11-21 13:38:08 +0800

    chore: enable static export and trailing slashes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "",
+  assetPrefix: "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
